456c5418e55827bcfc97485b353de71ae69a2514
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-12-06 11:05:09 +0000

    Updating to latest FlutterFlow output.

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.silver.pollweb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/silver/pollweb/pollweb/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.silver.pollweb.pollweb
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
